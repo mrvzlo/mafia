52801d10f8b6542690c0d883c7a7f87695c30df3
Vladimir Andrejev <andrejevve@gmail.com>
2023-01-16 20:42:52 +0200

    Remove extra

---
+++ src/components/map/Map.vue
@@ -0,0 +1,11 @@
+<template>HELLO</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+   props: {},
+})
+export default class Map extends Vue {}
+console.log(2);
+</script>
